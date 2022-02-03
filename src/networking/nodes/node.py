81aef5fe7e62df73085106c790fc87682723a6f0
Sam <samuelgardner101@gmail.com>
2022-02-03 02:52:03 +0000

    .

--- src/networking/nodes/node.py
+++ src/networking/nodes/node.py
@@ -10,14 +10,14 @@ class node:
     HOP_COUNT = 0
     IS_CLIENT = False
 
-    def __init__(self, relay_nodes: list[node] = [], auto_initialize=False):
+    def __init__(self, other_nodes: list[node] = [], auto_initialize=False):
         self._my_ephemeral_kex_keys: list[tuple[bytes, bytes]]
         self._other_nodes_info: list[node]
         self._shared_secrets: list[key_set]
         self._encapsulated_shared_secrets: list[bytes]
 
         self._my_ephemeral_kex_keys = [kex.generate_key_pair(self.IS_CLIENT) for _ in range(self.HOP_COUNT)]
-        self._other_nodes = relay_nodes
+        self._other_nodes = other_nodes
         self._shared_secrets = []
         self._encapsulated_shared_secrets = []
 
@@ -53,6 +53,16 @@ class node:
         check_hashes = map(lambda keys: keys.hashed_key, hashed_signed_shared_secrets)
         return all([constant_time.is_equal(known_hash, check_hash) for known_hash, check_hash in zip(known_hashes, check_hashes)])
 
+    def copy_single(self, index: int):
+        copy = node()
+        copy._my_ephemeral_kex_keys = self._my_ephemeral_kex_keys
+        copy._other_nodes = self._other_nodes
+        copy._shared_secrets = self._shared_secrets
+        copy._encapsulated_shared_secrets = self._encapsulated_shared_secrets
+
+        copy._my_ephemeral_kex_keys = [copy._my_ephemeral_kex_keys[index]]
+        return copy
+
 
 class client_node(node):
     HOP_COUNT = 3
@@ -72,23 +82,28 @@ if __name__ == "__main__":
 
     client = client_node([relay_node_1, relay_node_2, relay_node_3], auto_initialize=True)
 
-    relay_node_1._other_nodes = [client]; relay_node_1.initialize()
-    relay_node_2._other_nodes = [client]; relay_node_2.initialize()
-    relay_node_3._other_nodes = [client]; relay_node_3.initialize()
+    relay_node_1._other_nodes = [client.copy_single(0)]; relay_node_1.initialize()
+    relay_node_2._other_nodes = [client.copy_single(1)]; relay_node_2.initialize()
+    relay_node_3._other_nodes = [client.copy_single(2)]; relay_node_3.initialize()
 
     print("\nClient:")
     for key in client._my_ephemeral_kex_keys: print(key)
     print()
     for ss in client._shared_secrets: print(ss.master_key)
+    print()
+    for o in client._other_nodes: print(o._my_ephemeral_kex_keys)
 
     print("\nRN_1")
     for key in relay_node_1._my_ephemeral_kex_keys: print(key)
     for ss in relay_node_1._shared_secrets: print(ss.master_key)
+    for o in relay_node_1._other_nodes: print(o._my_ephemeral_kex_keys)
 
     print("\nRN_2")
     for key in relay_node_2._my_ephemeral_kex_keys: print(key)
     for ss in relay_node_2._shared_secrets: print(ss.master_key)
+    for o in relay_node_2._other_nodes: print(o._my_ephemeral_kex_keys)
 
     print("\nRN_3")
     for key in relay_node_3._my_ephemeral_kex_keys: print(key)
     for ss in relay_node_3._shared_secrets: print(ss.master_key)
+    for o in relay_node_3._other_nodes: print(o._my_ephemeral_kex_keys)
