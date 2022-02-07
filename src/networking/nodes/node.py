11d491926ebf10fac98f9fa1abcf0a249a1f5b4d
Sam <samuelgardner101@gmail.com>
2022-02-07 23:59:11 +0000

    .

--- src/networking/nodes/node.py
+++ src/networking/nodes/node.py
@@ -14,41 +14,29 @@ class node(abc.ABC):
     IS_CLIENT: bool = False
 
     def __init__(self, **kwargs):
-        self._my_static_signing_key_pair: key_pair = key_pair()  # signing.import_keypair("") TODO
-        self._their_static_signing_key_pairs: list[key_pair] = []
-        self._my_ephemeral_kex_key_pairs: list[key_pair] = []
-        self._their_ephemeral_kex_key_pais: list[key_pair] = []
-        self._shared_secrets: list[key_set] = []
-
         self._ip_address: ip = ip("")  # TODO -> securely determine own external ip address
         self._relay_nodes: list[node] = kwargs.get("relay_nodes", [])
         self._initialized: bool = kwargs.get("initialized", False)
 
-        if len(self._relay_nodes):
-            for relay_node in self._relay_nodes:
-                relay_node.initialize()
+        self._my_static_signing_key_pair: key_pair = key_pair()  # signing.import_keypair("") TODO
+        self._their_static_signing_key_pairs: list[key_pair] = []
+        self._my_ephemeral_kex_key_pairs: list[key_pair] = [kex.generate_key_pair(self.IS_CLIENT) for i in range(self.NUMBER_HOPS)]
+        self._their_ephemeral_kex_key_pairs: list[key_pair] = [relay_node.my_ephemeral_kex_key_pairs[0] for relay_node in self._relay_nodes]
+        self._shared_secrets: list[key_set] = []
+
+        for relay_node in self._relay_nodes:
+            relay_node.initialize()
 
     def initialize(self):
+        print(id(self), self._my_ephemeral_kex_key_pairs, self._their_ephemeral_kex_key_pairs)
         self._shared_secrets = [
             kex.compute_shared_secret(
                 my_ephemeral_kex_key_pair.secret_key,
                 their_ephemeral_kex_key_pair.public_key,
                 self.IS_CLIENT)
 
-            for my_ephemeral_kex_key_pair, their_ephemeral_kex_key_pair in zip(self._my_ephemeral_kex_key_pairs, self._their_ephemeral_kex_key_pais)
+            for my_ephemeral_kex_key_pair, their_ephemeral_kex_key_pair in zip(self._my_ephemeral_kex_key_pairs, self._their_ephemeral_kex_key_pairs)
         ]
-
-    @property
-    def my_static_signing_key_pair(self) -> key_pair:
-        return self._my_static_signing_key_pair
-
-    @property
-    def my_ephemeral_kex_key_pairs(self) -> list[key_pair]:
-        return self._my_ephemeral_kex_key_pairs
-
-    @property
-    def shared_secrets(self) -> list[key_set]:
-        return self._shared_secrets
 
     @property
     def ip_address(self) -> ip:
@@ -61,3 +49,15 @@ class node(abc.ABC):
     @property
     def initialized(self) -> bool:
         return self._initialized
+
+    @property
+    def my_static_signing_key_pair(self) -> key_pair:
+        return self._my_static_signing_key_pair
+
+    @property
+    def my_ephemeral_kex_key_pairs(self) -> list[key_pair]:
+        return self._my_ephemeral_kex_key_pairs
+
+    @property
+    def shared_secrets(self) -> list[key_set]:
+        return self._shared_secrets
