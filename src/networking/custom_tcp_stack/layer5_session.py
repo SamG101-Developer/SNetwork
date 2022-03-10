5710a5238cf1dc1c35adbf48ac493d4b16029a8c
Sam <samuelgardner101@gmail.com>
2022-03-10 14:07:21 +0000

    .

--- src/networking/custom_tcp_stack/layer5_session.py
+++
@@ -1,8 +0,0 @@
-class layer3_session:
-    @staticmethod
-    def initialize_connection(destination_address: str) -> None:
-        pass
-
-    @staticmethod
-    def maintain_heartbeat(destination_address: str) -> None:
-        pass
